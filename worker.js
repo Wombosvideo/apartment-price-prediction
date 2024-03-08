70ee8b523c8309e575a668bd463b396c482bca84
Luca Bosin <luca@bosin.ch>
2024-03-08 14:45:32 +0100

    Keep model in memory

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
-async function main(data) {
-  console.time();
+let pyodide = null;
 
+async function setup() {
   importScripts('//cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js');
 
   console.time('base64');
@@ -9,15 +9,13 @@ async function main(data) {
 
   const bfs_municipality_and_tax_data = await (await fetch('bfs_municipality_and_tax_data.csv', { mode: 'cors' })).text();
   const randomforest_regression = await (await fetch('randomforest_regression.pkl', { mode: 'cors' })).arrayBuffer();
-
-  console.log(bfs_municipality_and_tax_data);
   
   console.timeEnd('base64');
   console.time('pyodide');
 
   console.log("Starting Pyodide...");
 
-  let pyodide = await loadPyodide({
+  pyodide = await loadPyodide({
     packages: ['pandas', 'scikit-learn'],
   });
 
@@ -32,6 +30,44 @@ async function main(data) {
   pyodide.FS.writeFile('randomforest_regression.pkl', randomforest_regression_ia);
 
   console.timeEnd('files');
+  console.time('imports');
+
+  console.log("Importing libraries...");  
+
+  pyodide.runPython(`
+    import pandas as pd
+    from sklearn.ensemble import RandomForestRegressor
+    import pickle
+    import os
+  `);
+
+  console.timeEnd('imports');
+  console.time('load');
+
+  console.log("Loading data and model...");
+
+  pyodide.runPython(`
+    df_bfs_data = pd.read_csv('bfs_municipality_and_tax_data.csv', sep=',', encoding='utf-8')
+    df_bfs_data['tax_income'] = df_bfs_data['tax_income'].str.replace("'", "")
+
+    randomforest_model = RandomForestRegressor()
+    model_filename = "randomforest_regression.pkl"
+    with open(model_filename, 'rb') as f:
+        randomforest_model = pickle.load(f)
+  `);
+
+  console.timeEnd('load');
+
+  console.log("Setup done.");
+}
+
+async function main(data) {
+  console.time();
+
+  if (!pyodide) {
+    await setup();
+  }
+  
   console.time('model');
 
   const { bfs_number, area, rooms } = data;
@@ -39,23 +75,11 @@ async function main(data) {
   console.log("Running model...");
 
   pyodide.runPython(`
-    import pandas as pd
-    from sklearn.ensemble import RandomForestRegressor
-    import pickle
-    import os
-
     bfs_number = int(${bfs_number})
     area = float(${area})
     rooms = float(${rooms})
 
-    df_bfs_data = pd.read_csv('bfs_municipality_and_tax_data.csv', sep=',', encoding='utf-8')
-    df_bfs_data['tax_income'] = df_bfs_data['tax_income'].str.replace("'", "")
     df = df_bfs_data[df_bfs_data['bfs_number']==bfs_number]
-
-    randomforest_model = RandomForestRegressor()
-    model_filename = "randomforest_regression.pkl"
-    with open(model_filename, 'rb') as f:
-        randomforest_model = pickle.load(f)
 
     prediction = randomforest_model.predict([[rooms, area, df['pop'].iloc[0], df['pop_dens'].iloc[0], df['frg_pct'].iloc[0], df['emp'].iloc[0], df['tax_income'].iloc[0], area/rooms]])
     result = str(round(prediction[0],2))
